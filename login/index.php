36c9b2d0d9f0dee91e0d5ca2b2ddb94d7d9e451b
Noe Isaías <noechido1.nipd@gmail.com>
2018-11-29 15:49:08 -0600

    Deje todo como en el inicio "registrate"

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>INGRESATE</a>
+          	<a href="./php/registro.php"</a>REGISTRATE</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
