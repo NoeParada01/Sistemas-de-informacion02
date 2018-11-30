9366f47a099a01ec73bc0673a67a6fd0f71ae35b
JeshuaRL <chuyrey07@hotmail.com>
2018-11-30 10:07:16 -0600

    cambie la palabra ingresate

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>Inscribite</a>
+          	<a href="./php/registro.php"</a>Inscribe</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
