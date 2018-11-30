7fdae3268597a47ec4a0dcbe48024d00f443f37a
ciri18 <mitsubishiitalika18>
2018-11-30 16:27:49 -0600

    inscribir

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>inscripción</a>
+          	<a href="./php/registro.php"</a>inscripbir</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
