769c9605bd016744c4cf4dbbb0d07b4edbed748b
JeshuaRL <chuyrey07@hotmail.com>
2018-12-01 23:28:03 -0600

    cambie I. U. a  solo U S U A R I O

--- login/index.php
+++ login/index.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['session']))
 					<!--ejemplo <label for="usuario">Usuario:</label>-->
 					<!--required es nuevo en html5, si el campo está vacío te avisa, pero cuidado, no valida la información-->
 					<p><label >Usuario:</label></p>
-						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
+						<input name="usuario" type="text" id="usuario" placeholder="U S U A R I O" autofocus="" required=""></p>
 					
 					<p><label>contraseña:</label></p>
 						<input name="contrasenia" type="password" id="contrasenia" placeholder="Ingresa Password" required=""></p>
