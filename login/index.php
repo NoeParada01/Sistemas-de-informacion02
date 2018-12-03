f44e3b76536d3339d6890e81c926544ed0e43249
ciri18 <mitsubishiitalika18>
2018-12-02 22:59:24 -0600

    cambie el logo por una foto mia xd

--- login/index.php
+++ login/index.php
@@ -24,7 +24,7 @@ if(isset($_SESSION['session']))
 		<div id="contenedor">
 	
 			<div id="cabecera" >
-				<img src="./css/images/superm.jpg">
+				<img src="./css/images/Marvel-Hulk">
 			</div>
 	
 			<div id="cuerpo">
